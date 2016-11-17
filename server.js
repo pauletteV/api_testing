33fb58805553b085ef30d4301ba8b5dafd7c49b3
Paulette <pvallad@picnicnewmedia.com>
2016-11-17 18:59:26 -0500

    make changes to server logic

--- server.js
+++ server.js
@@ -4,22 +4,18 @@ var lyrics = require('./lyrics');
 var app = express();
 
 
-// respond with "Hello World!" on the homepage
-app.get('/api/lyrics', function (req, res) {
-//res.send('Hello World!');
-res.send(lyrics);
+app.get('/fullLyrics', function (req, res) {
+	res.send(lyrics);
 });
+
+app.get('/randomlyrics', function (req, res) {
+	var chosenString = lyrics[Math.floor(Math.random()*lyrics.length)];
+	res.send(chosenString);
+});
+
 var server = app.listen(3000, function () {
-var host = server.address().address;
-var port = server.address().port;
-console.log('Example app listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
 });
 
-
-/*http.createServer(function(request, response) {
-  var chosenString = lyrics[Math.floor(Math.random()*lyrics.length)];
-
-  response.writeHead(200, { "Content-type": "text/plain" });
-  response.write(chosenString);
-  response.end();
-}).listen(1234); //this is the port the server is on; can be 8888 or 8080 for localhost*/
